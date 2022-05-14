24dc8a7d901d734638891c401c3414879903b476
JaviZamudio <javier.elihu100@gmail.com>
2022-05-14 18:44:48 -0500

    adding cors

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express()
+
+app.use(cors());
 
 app.get("/", (req, res) => {
     res.send("Hello World!")
 })
+
 
 const PORT = process.env.PORT || 4000
 app.listen(PORT, () => {
